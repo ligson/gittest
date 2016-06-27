a9d6318e17c8b68ea24179bd7848ca9b25598ec8
ligson <593949938@qq.com>
2016-06-27 12:12:04 +0800

    项目初始化

---
+++ src/main/java/org/ligson/gitteste/Test.java
@@ -0,0 +1,10 @@
+package org.ligson.gitteste;
+
+/**
+ * Created by trq on 2016/6/27.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world............");
+    }
+}
